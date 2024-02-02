53810dcbcc38a357f09010aa21d2937a4ffbbed4
TjensenRex <trex35@sbcglobal.net>
2024-02-02 15:31:19 -0700

    last for Milestone 3

--- app/src/main/java/edu/byu/cs/tweeter/client/model/service/backgroundTask/LoginTask.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/model/service/backgroundTask/LoginTask.java
@@ -10,11 +10,9 @@ import edu.byu.cs.tweeter.util.Pair;
  * Background task that logs in a user (i.e., starts a session).
  */
 public class LoginTask extends AuthenticateTask {
-
     public LoginTask(String username, String password, Handler messageHandler) {
         super(messageHandler, username, password);
     }
-
     @Override
     protected Pair<User, AuthToken> runAuthenticationTask() {
         User loggedInUser = getFakeData().getFirstUser();
